7afcac004d5088e1f91a731ad9f13fab926918f8
Anthony Seager <Anthony@seager.com.au>
2022-05-24 20:45:47 +0930

    Implemented Odd-Even sort

--- Sorting Algorithms/Exchanging/odd_even_sort.cpp
+++ Sorting Algorithms/Exchanging/odd_even_sort.cpp
@@ -6,6 +6,28 @@
 // odd-even sort algorithm
 void oddEvenSort(std::vector<int> *vec) {
 
+    bool sorted = false;
+
+    while (!sorted) {
+        
+        sorted = true; 
+
+        // Bubble sort on odd indices
+        for (int i = 1; i < vec->size() - 1; i += 2) {
+            if (vec->at(i) > vec->at(i + 1)) {
+                std::swap(vec->at(i), vec->at(i + 1));
+                sorted = false;
+            }
+        }
+
+        // Bubble sort on even indices
+        for (int i = 0; i < vec->size() - 1; i += 2) {
+            if (vec->at(i) > vec->at(i + 1)) {
+                std::swap(vec->at(i), vec->at(i + 1));
+                sorted = false;
+            }
+        }
+    }
 }
 
 int main() { 
