9f544357235266312f03b3a3c772d4de0a5172e8
Anthony Seager <32993852+mo0nmann@users.noreply.github.com>
2022-05-24 13:00:39 +0930

    Implemented exchange sort

--- Sorting Algorithms/Exchanging/exchange_sort.cpp
+++ Sorting Algorithms/Exchanging/exchange_sort.cpp
@@ -6,6 +6,14 @@
 // exchange sort algorithm
 void exchangeSort(std::vector<int> *vec) {
 
+    for (int i = 0; i < vec->size() - 1; i++) {
+        for (int j = i + 1; j < vec->size(); j++) {
+            if (vec->at(i) > vec->at(j)) {
+                std::swap(vec->at(i), vec->at(j));
+            }
+        }
+    }
+
 }
 
 int main() { 
