a7e9dc8943bf812533bc6eb9e65ea499926bc2cb
Anthony Seager <Anthony@seager.com.au>
2022-05-26 03:33:54 +0930

    Correctly Implemented insertion method
    Changed the insertion sort from using swaps to actually shifting elements and inserting. (Oops for before)

--- Sorting Algorithms/Insertion/insertion_sort.cpp
+++ Sorting Algorithms/Insertion/insertion_sort.cpp
@@ -3,16 +3,28 @@
 
 #include "../helpers.hpp"
 
+#include <chrono>
+
 // insertion sort algorithm
 void insertionSort(std::vector<int> *vec) {
+
+    int key, j;
 
     // move from the second element to the last
     for (int i = 1; i < vec->size(); i++) {
 
-        // compare and swap with the previous element if it is larger
-        for (int j = i; j > 0 && vec->at(j) < vec->at(j - 1); j--) {
-            std::swap(vec->at(j), vec->at(j - 1));
+        /* hold the element that we want to compare and
+           potentially insert into the correct place */
+        key = vec->at(i);
+
+        /* move each element on the left of the key that 
+           is larger to the right                     */
+        for (j = i - 1; j >= 0 && vec->at(j) > key; j--) {
+            vec->at(j + 1) = vec->at(j);
         }
+
+        // insert the element into it's correct position
+        vec->at(j + 1) = key;
     }
     
 }
@@ -30,6 +42,7 @@ int main() {
 
     // insertion sort
     insertionSort(&sorted_vector);
+    
     // output
     printComparison(unsorted_vector, sorted_vector);
     
